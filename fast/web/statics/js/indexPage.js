f1922975fbb73cf6457412fc0bb55798b833b422
Dalmo Felipe <dalmo.felipe@gmail.com>
2024-01-27 11:41:21 -0300

    frontend ajusts layout

--- fast/web/statics/js/indexPage.js
+++ fast/web/statics/js/indexPage.js
@@ -3,11 +3,4 @@ document.addEventListener("DOMContentLoaded", StartIndexPage)
 
 function StartIndexPage() {
     console.log('Script IndexPage carregado!');
-
-    let smallTime = document.getElementById('time')
-    smallTime.innerHTML = new Date().toLocaleString('pt-BR')
-
-    setInterval(() => {
-        smallTime.innerHTML = new Date().toLocaleString('pt-BR')
-    }, 1000)
 }
